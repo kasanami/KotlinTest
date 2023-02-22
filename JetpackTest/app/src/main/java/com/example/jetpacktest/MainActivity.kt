2826f36fe7e02c95b22f53e4b97441d75d8d02b0
笠浪貴大 <dev.kacha@gmail.com>
2023-02-22 17:22:49 +0900

    テキストの装飾テスト

--- JetpackTest/app/src/main/java/com/example/jetpacktest/MainActivity.kt
+++ JetpackTest/app/src/main/java/com/example/jetpacktest/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.jetpacktest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpacktest.ui.theme.JetpackTestTheme
 
@@ -44,6 +48,21 @@ fun AppScreen() {
         Text(text = story, color = Color.Red)
         Text(text = story, color = Color.Blue, maxLines = 2)
         Text(text = story, color = Color.Green, maxLines = 1)
+
+        Text(
+            text = "Hello",
+            modifier = Modifier
+                .background(Color.Gray)
+                .border(1.dp, Color.Red)
+                .padding(5.dp)
+        )
+
+        Text(
+            text = "Hello",
+            modifier = Modifier
+                .background(color = Color(0xff66cdaa))
+                .padding(20.dp)
+        )
     }
 }
 
